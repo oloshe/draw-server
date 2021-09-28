f84c41a77064d2a624dd28256be620c4a97a174a
oloshe <jacob.j@qq.com>
2021-09-28 21:02:07 +0800

    update

--- app/src/http.rs
+++ app/src/http.rs
@@ -1,10 +1,39 @@
 use actix_web::{HttpResponse, post, web};
 use serde::Deserialize;
 
+use crate::data_struct::base::RoomId;
 use crate::data_struct::{player::PlayerData, room::RoomInfo};
-use crate::msg::message::{LobbyCreateRoom};
+use crate::msg::message::{LobbyCreateRoom, LobbyJoinRoom};
 use crate::LOBBY_ADDR as lobby;
 
+#[derive(Debug,Deserialize)]
+pub struct ImportedData {
+	pub player: PlayerData,
+}
+
+#[post("/join/{room_id}")]
+pub(crate) async fn join_room(room_id: web::Path<String>, data: web::Json<ImportedData>) -> HttpResponse {
+	let ret = lobby.send(LobbyJoinRoom {
+		player_data: data.0.player,
+		room_id: RoomId(room_id.to_string()),
+	}).await;
+	match ret {
+		Ok(ret) => {
+			if let Some(reason) = ret {
+				HttpResponse::Ok()
+					.body(reason)
+			} else {
+				HttpResponse::Ok()
+					.body("ok")
+			}
+		},
+		Err(e) => {
+			utils::error!("{}", e);
+			HttpResponse::InternalServerError()
+				.body("fail")
+		}
+	}
+}
 
 #[derive(Debug,Deserialize)]
 pub struct CreateRoomInfo {
